630156d7eb5751aa22aed89d8b39505e5b2db1cb
VirtuBox <contact@virtubox.net>
2017-12-07 20:36:52 +0100

    Update nginx-build.sh

--- nginx-build.sh
+++ nginx-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt-get install -y build-essential libtool automake autoconf zlib1g-dev libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
+
+apt-get install -y build-essential libtool automake autoconf zlib1g-dev libpcre3-dev libgd-dev libssl-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
+
+rm -rf /usr/local/src/*
 cd /usr/local/src
-wget http://nginx.org/download/nginx-1.13.7.tar.gz
-tar -xzvf nginx-1.13.7.tar.gz
-mv nginx-1.13.7 nginx
 
 git clone https://github.com/FRiCKLE/ngx_cache_purge.git
 git clone https://github.com/openresty/memc-nginx-module.git
@@ -16,6 +16,7 @@ git clone https://github.com/openresty/srcache-nginx-module.git
 git clone https://github.com/openresty/set-misc-nginx-module.git
 git clone https://github.com/FRiCKLE/ngx_coolkit.git
 git clone https://github.com/FRiCKLE/ngx_slowfs_cache.git
+
 wget https://people.freebsd.org/~osa/ngx_http_redis-0.3.8.tar.gz
 tar -zxf ngx_http_redis-0.3.8.tar.gz
 mv ngx_http_redis-0.3.8 ngx_http_redis
@@ -23,16 +24,23 @@ mv ngx_http_redis-0.3.8 ngx_http_redis
 git clone https://github.com/google/ngx_brotli.git
 cd ngx_brotli
 git submodule update --init --recursive
+
 cd /usr/local/src
 
 git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout tls1.3-draft-18
 
+cd /usr/local/src/
+
+wget http://nginx.org/download/nginx-1.13.7.tar.gz
+tar -xzvf nginx-1.13.7.tar.gz
+mv nginx-1.13.7 nginx
+
 cd /usr/local/src/nginx/
+
 wget https://raw.githubusercontent.com/cujanovic/nginx-dynamic-tls-records-patch/master/nginx__dynamic_tls_records_1.11.5%2B.patch
 patch -p1 < nginx__dynamic_tls_records_1.11.5*.patch
-
 
 ./configure \
  --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2' \
@@ -48,7 +56,6 @@ patch -p1 < nginx__dynamic_tls_records_1.11.5*.patch
  --http-proxy-temp-path=/var/lib/nginx/proxy \
  --http-scgi-temp-path=/var/lib/nginx/scgi  \
  --http-uwsgi-temp-path=/var/lib/nginx/uwsgi  \
- --with-debug  \
  --with-pcre-jit  \
  --with-http_ssl_module  \
  --with-http_stub_status_module  \
@@ -60,8 +67,8 @@ patch -p1 < nginx__dynamic_tls_records_1.11.5*.patch
  --with-http_image_filter_module  \
  --with-http_v2_module  \
  --with-http_sub_module  \
+ --with-http_xslt_module  \
  --with-threads  \
- --without-http_rewrite_module \
  --add-module=/usr/local/src/ngx_cache_purge  \
  --add-module=/usr/local/src/memc-nginx-module \
  --add-module=/usr/local/src/ngx_devel_kit  \
@@ -76,12 +83,14 @@ patch -p1 < nginx__dynamic_tls_records_1.11.5*.patch
  --with-openssl=/usr/local/src/openssl \
  --with-openssl-opt=enable-tls1_3 \
  --sbin-path=/usr/sbin/nginx 
-
+ 
 make -j $(nproc)
 make install
 sudo systemctl unmask nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
-wget -O /etc/nginx/nginx.conf https://git.virtubox.net/virtubox/ee/raw/master/nginx.conf
 nginx -t && service nginx reload
+
+
+
 sudo systemctl restart nginx
