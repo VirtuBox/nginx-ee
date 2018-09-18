b659f83463ac93f72a3b2560fbfcd01d3358c853
VirtuBox <contact@virtubox.net>
2018-09-18 17:58:40 +0200

    fix binary operator

--- nginx-build.sh
+++ nginx-build.sh
@@ -184,7 +184,7 @@ fi
 # install gcc-7
 distro_version=$(lsb_release -sc)
 
-if [[ "$NGINX_RELEASE" == "1" && "$RTMP" !== "y" ]]; then
+if [[ "$NGINX_RELEASE" == "1" && "$RTMP" != "y" ]]; then
     if [[ "$distro_version" == "xenial" || "$distro_version" == "bionic" ]]; then
         if [[ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-bionic.list && ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-xenial.list ]]; then
             echo -ne '       Installing gcc-8                       [..]\r'
@@ -544,7 +544,7 @@ fi
 echo -ne '       Configuring nginx                      [..]\r'
 
 if [[ "$distro_version" == "xenial" || "$distro_version" == "bionic" ]]; then
-	if [[ "$NGINX_RELEASE" == "1" && "$RTMP" !== "y" ]]; then
+	if [[ "$NGINX_RELEASE" == "1" && "$RTMP" != "y" ]]; then
 		export CC="/usr/bin/gcc-8"
 		export CXX="/usr/bin/gc++-8"
 	else
