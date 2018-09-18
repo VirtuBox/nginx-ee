3f615a79de7007c17b51f247ea0deedfab9ce648
VirtuBox <contact@virtubox.net>
2018-09-18 17:54:44 +0200

    fix gcc8 install check

--- nginx-build.sh
+++ nginx-build.sh
@@ -184,7 +184,7 @@ fi
 # install gcc-7
 distro_version=$(lsb_release -sc)
 
-if [[ "$NGINX_RELEASE" == "1" && "$RTMP" == "n" ]]; then
+if [[ "$NGINX_RELEASE" == "1" && "$RTMP" !== "y" ]]; then
     if [[ "$distro_version" == "xenial" || "$distro_version" == "bionic" ]]; then
         if [[ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-bionic.list && ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-xenial.list ]]; then
             echo -ne '       Installing gcc-8                       [..]\r'
