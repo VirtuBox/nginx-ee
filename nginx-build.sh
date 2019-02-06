28b64fbc6f5762c83d63ae8d15c9694a8316bea5
VirtuBox <contact@virtubox.net>
2019-02-06 18:19:31 +0100

    new hpack patch

--- nginx-build.sh
+++ nginx-build.sh
@@ -740,13 +740,12 @@ echo -ne '       Applying nginx patches                 [..]\r'
 if [ "$NGINX_RELEASE" = "1" ]; then
     {
 
-        curl -sL https://raw.githubusercontent.com/nginx-modules/ngx_http_tls_dyn_size/master/nginx__dynamic_tls_records_1.15.5%2B.patch | patch -p1
-        curl -sL https://raw.githubusercontent.com/centminmod/centminmod/123.09beta01/patches/cloudflare/nginx-1.15.3_http2-hpack.patch | patch -p1
-        curl -sL https://raw.githubusercontent.com/kn007/patch/master/nginx_auto_using_PRIORITIZE_CHACHA.patch | patch -p1
+        curl -sL https://github.com/centminmod/centminmod/blob/123.09beta01/patches/cloudflare/nginx__dynamic_tls_records_1015008.patch | patch -p1
+        curl -sL https://raw.githubusercontent.com/centminmod/centminmod/123.09beta01/patches/cloudflare/nginx-1.15.8_http2-hpack.patch | patch -p1
     } >>/tmp/nginx-ee.log 2>&1
 
 else
-    curl -sL https://raw.githubusercontent.com/nginx-modules/ngx_http_tls_dyn_size/master/nginx__dynamic_tls_records_1.13.0%2B.patch | patch -p1 >>/tmp/nginx-ee.log 2>&1
+    curl -sL https://raw.githubusercontent.com/centminmod/centminmod/123.09beta01/patches/cloudflare/nginx_1.13.1_http2_hpack.patch | patch -p1 >>/tmp/nginx-ee.log 2>&1
 fi
 
 if [ $? -eq 0 ]; then
